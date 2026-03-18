14584d41e9aee923c6b598fedbeedbbea6c1be03
AyabongaGcina <4446494@myuwc.ac.za>
2026-03-18 12:24:39 +0000

    Added sourceFilePath and destinationFilePath

--- ulysess_cleaned.java
+++ ulysess_cleaned.java
@@ -4,3 +4,9 @@
 
 import java.io.*;
 
+public class ulysess_cleaned {
+    public static void main(String[] args) {
+        String sourceFilePath = "ulysses.txt";
+        String destinationFilePath = "cleaned_ulysses.txt";
+        
+        int totalCleanWords = 0;
